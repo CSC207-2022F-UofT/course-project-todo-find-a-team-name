09f3205f19412dfd249e2e05968782ce6a09087f
YahyaElgabra <elgabrayahya@gmail.com>
2022-11-14 16:06:25 -0500

    add Block and started on BlockTest

---
+++ src/test/java/entities/BlockTest.java
@@ -0,0 +1,22 @@
+package entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockTest {
+
+    @org.junit.jupiter.api.Test
+    void getDay() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStartTime() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEndTime() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRoom() {
+    }
+}
