f22aea7c069b3b79692b3d7da1802ce132c76524
Firebrandt <jumpyflax797@gmail.com>
2022-11-15 13:06:38 -0500

    Quickly re-arrange the Course file to the correct position in the java folder.

--- src/main/java/entities/Course.java
+++ src/main/java/entities/Course.java
@@ -1,23 +1,22 @@
+package entities;
+
 /** An entity representing a Course.
- *
  * Strings are used for tags because it's convenient with JSON. Don't actually use them as strings!
- *
  * Instance Attributes:
  * - sections: A list of all sections in the Course.
  * - Contains the course session (First, Second).
  * - Contains the course code, and BR category.
  * - title: Title of the Course
- *
  * BR is sometimes an empty string. There should be a way to handle this somewhere.
  * This shouldn't change after initialization.
  */
 public abstract class Course() {
 
-    private String title;
-    private List<Section> sections;
-    private String courseSession;
-    private String courseCode;
-    private String breadth;
+    private final String title;
+    private final List<Section> sections;
+    private final String courseSession;
+    private final String courseCode;
+    private final String breadth;
 
     Course(String title, List<Section> sections, String courseSession, String courseCode, String breadth){
         this.title = title;
---
+++ src/main/screens/InputDialog.java
@@ -0,0 +1,17 @@
+/**
+ * A class storing the attributes and the creation mechanism for an input dialog. Specifically for overlap max.
+ * Note that the view must be drawn/presented by using a showXXXdialog() method, or by setting it visible manually.
+
+ Inherits from JOptionPane -- which is basically our InputDialog abstract class (now obsolete).
+
+ Also determines how it's drawn at the moment. I think the presenter can be the one that actually draws it,
+ but this one will determine basically the layout.
+ * **/
+
+class InputDialog() : JDialog() {
+
+    JDialog(the one that uses a contentpane)
+
+        JPanel Content
+
+}
