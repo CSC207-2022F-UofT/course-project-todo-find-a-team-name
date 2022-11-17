d7e1f605a14d159223c5c5059b5b6366dfbb3caa
Michael Hui <michaelsjhui@gmail.com>
2022-11-17 01:03:12 -0500

    Implementing EditCourseScreen.

--- src/main/java/screens/EditCourseScreen.java
+++ src/main/java/screens/EditCourseScreen.java
@@ -1,19 +1,28 @@
 package screens;
 
 import edit_timetable_use_case.EditTimetableController;
+import entities.Section;
 
 import javax.swing.*;
 
 public class EditCourseScreen extends JPanel {
 
     private EditTimetableScreen screen;
+    private Controller controller;
     private String courseCode;
 
-    public EditCourseScreen(EditTimetableScreen screen, , String courseCode){
+    public EditCourseScreen(EditTimetableScreen screen, ModuleLayer.Controller controller, String courseCode){
         this.screen = screen;
         this.courseCode = courseCode;
+        this.controller = controller;
 
+        CourseResponseModel calCourse = controller.retrieveCalendarCourseData(courseCode);
+        CourseResponseModel course = controller.retrieveTimetableCourseData(courseCode);
 
+        for (Section section : calCourse.getSections()){
+            ... generate a box with data...
+
+        }
 
         screen.setVisible(false);
     }
