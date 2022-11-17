b58e10f6dbaa011d961a032d252f6f88d05ac02e
hanshhh <wenqi.zhan@mail.utoronto.ca>
2022-11-17 01:43:12 -0500

    Implemented WeekdayConstraint.java

---
+++ src/main/java/entities/WeekdayConstraint.java
@@ -0,0 +1,64 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * An entity representing an TimeConstraint.
+ *
+ * Instance Attributes:
+ * - days: A List of days.
+ * - super(isBlackList): a boolean showing b/w lists.
+ *
+ * Note: every weekday from Monday to Friday is represented by an integer from 0 to 4.
+ */
+public class WeekdayConstraint extends Constraint{
+    private final List<Integer> days;
+
+    public WeekdayConstraint(List<Integer> days, boolean isBlackList) {
+        super(isBlackList);
+        this.days = days;
+    }
+
+    /**
+     * filter and modify the sections instance variable of a Course Object based on the
+     * given weekday.
+     *
+     * @param course the course to be modified with filtered sections.
+     */
+    @Override
+    public void filter(CalendarCourse course) {
+        ArrayList<Section> copy = new ArrayList<>(course.getSections());
+        for (Section section : copy) {
+            if (this.evalRemoveSectionCondition(this.evalBlackListFilterCondition(section))) {
+                course.removeSection(section);
+            }
+        }
+    }
+
+    /**
+     * a helper method that loop through the blocks of a section to evaluate the whether the section should be
+     * removed if the Weekday Constraint is a blacklist.
+     *
+     * @param section a section entity
+     * @return a boolean indicating the RemoveCondition of a BlackList.
+     */
+    private boolean evalBlackListFilterCondition(Section section) {
+        for (Block block : section.getBlocks()){
+            if (days.contains(block.getDay())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Integer> getWeekdays() {
+        return days;
+    }
+
+    @Override
+    public String toString(){
+        return "Weekday " + super.toString() + ": " + days;
+    }
+
+
+}
