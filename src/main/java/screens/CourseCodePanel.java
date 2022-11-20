df00bbe58e5ba18a4b044ad9cd421d3f4f58fba2
hanshhh <wenqi.zhan@mail.utoronto.ca>
2022-11-20 00:54:08 -0500

    [+] implemented ConstraintsInputScreen component.

---
+++ src/main/java/screens/CourseCodePanel.java
@@ -0,0 +1,20 @@
+package screens;
+
+import javax.swing.*;
+
+/**
+ * ConstraintsInputScreen UI component: a panel used for course codes input.
+ */
+public class CourseCodePanel extends JPanel{
+
+    JLabel label;
+    JTextField textField;
+
+    public CourseCodePanel(JLabel label, JTextField textField) {
+        this.label = label;
+        this.add(label);
+        this.textField = textField;
+        this.add(textField);
+    }
+
+}
