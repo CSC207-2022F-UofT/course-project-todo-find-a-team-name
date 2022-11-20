a0f82bd733aee2e29de6e3e9944bbf68115f52cb
Firebrandt <jumpyflax797@gmail.com>
2022-11-20 14:11:28 -0500

    Implemented java's Flow interfaces to (kind of) get bastardized communication between the controller and the dialog.

--- src/main/java/overlap_crap_fix_locations_later/DialogPresenter.java
+++ src/main/java/overlap_crap_fix_locations_later/DialogPresenter.java
@@ -1,4 +1,11 @@
 package overlap_crap_fix_locations_later;
 
+import java.util.ArrayList;
+
+/** Presenter that creates an appropriate OverlapInput for the OverlapMaximizationController. */
 public class DialogPresenter {
+
+    public void createDialog(OverlapInputFunctionality overlapInputWindow, ArrayList<String> scheduleList){
+
+    }
 }
--- src/main/java/overlap_crap_fix_locations_later/OverlapMaximizationController.java
+++ src/main/java/overlap_crap_fix_locations_later/OverlapMaximizationController.java
@@ -1,4 +1,47 @@
 package overlap_crap_fix_locations_later;
 
-public class OverlapMaximizationController {
+import java.util.ArrayList;
+import java.util.concurrent.Flow;
+
+public class OverlapMaximizationController implements Flow.Subscriber {
+
+    public void performUseCase(){
+        ArrayList<String> testItems = new ArrayList<>();
+
+    }
+
+    public static void main(String[] args) {
+        OverlapInputDialog dialog = new OverlapInputDialog(new String[]{"A", "B", "C"});
+        dialog.subscribe(new OverlapMaximizationController());
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+
+    // TODO: For the moment, this is a string for testing. Change it later.
+    public String selectedTimetable;
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        subscription.request(1);
+
+    }
+
+    @Override
+    public void onNext(Object item) {
+        selectedTimetable = (String) item;
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        System.out.println("Hey, we got an error from the OverlapMaximization's associated publisher. Check it out.");
+    }
+
+    @Override
+    /** Runs the rest of the use case once we actually have the data. This will end up being called from the
+     * ScheduleSelectInterface interface that we're calling from.
+     */
+    public void onComplete() {
+        System.out.println(selectedTimetable);
+        // Do nothing. I mean, we only needed one event.
+    }
 }
