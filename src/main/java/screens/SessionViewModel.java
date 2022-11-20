7ed3be937a660c76ac381dfb9848d752ca8e04ea
Michael Hui <michaelsjhui@gmail.com>
2022-11-20 16:40:46 -0500

    TimetableResponseConverter converts from any given Timetable datatype ResponseModel to the corresponding ViewModel.

---
+++ src/main/java/screens/SessionViewModel.java
@@ -0,0 +1,24 @@
+package screens;
+
+import entities.CalendarCourse;
+
+import java.util.HashMap;
+
+public class SessionViewModel {
+
+    private HashMap<String, TimetableViewCourseModel> courses;
+    private String sessionType;
+
+    public SessionViewModel(HashMap<String, TimetableViewCourseModel> courses, String sessionType){
+        this.courses = courses;
+        this.sessionType = sessionType;
+    }
+
+    public HashMap<String, TimetableViewCourseModel> getCourses() {
+        return courses;
+    }
+
+    public String getSessionType() {
+        return sessionType;
+    }
+}
