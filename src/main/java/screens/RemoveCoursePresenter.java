c83766764a232fe1788b5f58cba70e2c44672663
Michael Hui <michaelsjhui@gmail.com>
2022-11-20 14:17:44 -0500

    Implementing EditTimetableView interface

--- src/main/java/screens/RemoveCoursePresenter.java
+++ src/main/java/screens/RemoveCoursePresenter.java
@@ -9,10 +9,16 @@ import edit_timetable_use_case.RemoveCourseFailedException;
  */
 public class RemoveCoursePresenter implements RemoveCourseOutputBoundary {
 
+    private EditTimetableView screen;
+
     /**
      * @param responseModel
      * @return
      */
+
+    public RemoveCoursePresenter(EditTimetableView view){
+        this.view = view;
+    }
     @Override
     public EditTimetableResponseModel prepareView(EditTimetableResponseModel responseModel) throws RemoveCourseFailedException {
         if (responseModel.isSuccess()){
