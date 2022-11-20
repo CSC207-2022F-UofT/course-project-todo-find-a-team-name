d2f6308eb0a8c99c1afd8cba2fb19e09922a2f0e
JD-Grimm <jd.grimm@mail.utoronto.ca>
2022-11-19 19:25:45 -0500

    Added timetable entity.

---
+++ src/main/java/entities/Timetable.java
@@ -0,0 +1,25 @@
+package entities;
+import java.util.*;
+
+/** An implementation of the timetable which stores all of the timetablecourses
+ * each timetable block either is empty or corresponds to a Timetable course.
+ */
+public class Timetable {
+    private ArrayList<TimetableCourse> CourseList;
+    public Timetable(ArrayList<TimetableCourse> timetableCourses){
+        this.CourseList = new ArrayList<TimetableCourse>();
+        this.CourseList.addAll(timetableCourses);
+        }
+    public TimetableCourse getCourse(String code){
+        for(TimetableCourse course: this.CourseList){
+            if(course.courseCode.equals(code)){
+                return course;
+            }
+        }
+        return null;
+    }
+
+
+    }
+
+
