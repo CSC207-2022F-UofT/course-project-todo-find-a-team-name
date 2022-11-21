da298c7ed00cbcc6c1f874e4103615898833651a
hanshhh <wenqi.zhan@mail.utoronto.ca>
2022-11-21 18:00:49 -0500

    Create MVC, Implemented View, ViewModel, Controller

---
+++ src/main/java/screens/SectionFilterController.java
@@ -0,0 +1,30 @@
+package screens;
+
+import blacklist_whitelist_use_case.SectionFilterInputBoundary;
+import blacklist_whitelist_use_case.SectionFilterRequestModel;
+
+/**
+ * Controller for BlackListWhiteListUseCase responsible for requesting the interactor to apply those constraints
+ * to filter out the sections that fails the constraint.
+ */
+public class SectionFilterController {
+    private final SectionFilterInputBoundary sectionFilterInteractor;
+
+    /**
+     * Constructs SectionFilterController with a passed in SectionFilterInteractor that implements the
+     * SectionFilterInputBoundary.
+     * @param sectionFilterInteractor blacklist/whitelist use case interactor responsible for filtering out the
+     *                                unwanted sections of a calendar course after applying constraints.
+     */
+    public SectionFilterController(SectionFilterInputBoundary sectionFilterInteractor){
+        this.sectionFilterInteractor = sectionFilterInteractor;
+    }
+
+    /**
+     * Filter out the unwanted sections of a calendar course after applying constraints and display to the user.
+     * @param requestModel Data Structure representing the input data from the user for Blacklist/Whitelist use case.
+     */
+    public void filter(SectionFilterRequestModel requestModel){
+        sectionFilterInteractor.filter(requestModel);
+    }
+}
