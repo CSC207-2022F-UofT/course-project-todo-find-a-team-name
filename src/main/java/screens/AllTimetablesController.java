7796b1fb52f90b0516b3c7545105fe8d13e7db55
YahyaElgabra <elgabrayahya@gmail.com>
2022-11-20 23:22:43 -0500

    add TODO notes for unfinished parts finish most of Sorter use case. Only SorterInteractor and SorterPresenter remain, but interactor uses viewmodel which violates clean architecture AllTimetablesScreen is a standalone screen that is unresponsive, next step is to send User to Main Menu, TimeTableUI, and SorterScreen when appropriate buttons are clicked

--- src/main/java/screens/AllTimetablesController.java
+++ src/main/java/screens/AllTimetablesController.java
@@ -1,5 +1,8 @@
 package screens;
 
+/**
+ * TODO this and all other parts of AllTimetables use case, it should connect to other screens once buttons are clicked
+ */
 public class AllTimetablesController {
 
 
