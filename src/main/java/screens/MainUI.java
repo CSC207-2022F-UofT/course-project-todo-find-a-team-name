a72646e3bf60e2bc85c515c3897b718274c6de5c
Kai15116 <kai.iharantonio@gmail.com>
2022-11-25 17:14:19 -0500

    Added documentation to MainUI

--- src/main/java/screens/MainUI.java
+++ src/main/java/screens/MainUI.java
@@ -7,28 +7,35 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Class used to display the main menu of this program that allow user to import files and navigates user to
+ * other screens.
+ * Instance Attributes:
+ *      - filePathSession: JLabel representing the filepath of the session data that user imports.
+ *      If user have not selected the filepath, it's text becomes "Choose the file..."
+ *      - filePathTimetable: filepath of the timetable data user imports
+ *      If user have not selected the filepath, it's text becomes "Choose the file..."
+ */
 public class MainUI extends JPanel implements ActionListener {
 
     private final JLabel filePathSession;
     private final JLabel filePathTimetable;
 
+    /**
+     * Constructs MainUI with title, import timetable/session buttons, and display/edit/generate timetable buttons.
+     */
     public MainUI(){
         super();
         setLayout(new BorderLayout());
-
 
         JLabel title = new JLabel("Main menu");
         title.setHorizontalAlignment(JLabel.CENTER);
         title.setFont(title.getFont().deriveFont(18F));
         add(title, BorderLayout.PAGE_START);
 
-
         JPanel centerPanel = new JPanel();
-
-
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
 
         JPanel timetablePanel = new JPanel();
         timetablePanel.setLayout(new BoxLayout(timetablePanel, BoxLayout.PAGE_AXIS));
@@ -84,6 +91,12 @@ public class MainUI extends JPanel implements ActionListener {
         add(importSessionPanel, BorderLayout.PAGE_END);
     }
 
+    /**
+     * Process the user's interaction with buttons by navigating to appropriate screen/window when
+     * button is pressed by user.
+     *
+     * @param e the event to be processed
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
