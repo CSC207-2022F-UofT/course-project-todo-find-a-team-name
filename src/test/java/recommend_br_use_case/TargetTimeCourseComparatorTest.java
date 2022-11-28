7b4c6948c719ee04b0d6face2b025e7877175a6f
Kai15116 <kai.iharantonio@gmail.com>
2022-11-28 17:53:12 -0500

    Added test classes

---
+++ src/test/java/recommend_br_use_case/TargetTimeCourseComparatorTest.java
@@ -0,0 +1,12 @@
+package recommend_br_use_case;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TargetTimeCourseComparatorTest {
+
+    @Test
+    void testCompare() {
+    }
+}
