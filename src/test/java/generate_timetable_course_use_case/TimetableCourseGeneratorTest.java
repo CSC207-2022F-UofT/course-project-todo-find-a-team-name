164b8193a9800a581ba104202efa78576c57bbb7
Kai15116 <kai.iharantonio@gmail.com>
2022-11-29 13:10:40 -0500

    Added test for generateAllTimetableCourses with argument

--- src/test/java/generate_timetable_course_use_case/TimetableCourseGeneratorTest.java
+++ src/test/java/generate_timetable_course_use_case/TimetableCourseGeneratorTest.java
@@ -2,6 +2,7 @@ package generate_timetable_course_use_case;
 
 import entities.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -11,41 +12,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TimetableCourseGeneratorTest {
 
-    @Test
-    void testGenerateAllTimetableCoursesWithoutArgument() {
+    static Section lec0101;
+    static Section lec0201;
+    static Section tut0101;
+    static Section tut0201;
+    static Section pra0101;
+    static CalendarCourse calCourse;
+
+    @BeforeAll
+    static void setUp(){
         List<Section> sections = new ArrayList<>();
 
         List<Block> blocks1 = new ArrayList<>();
         blocks1.add(new Block("MO", "12:00", "13:00", ""));
         blocks1.add(new Block("TU", "14:00", "16:00", ""));
-        Section lec0101 = new Section("LEC-0101", "inst1", blocks1);
+        lec0101 = new Section("LEC-0101", "inst1", blocks1);
         sections.add(lec0101);
 
         List<Block> blocks2 = new ArrayList<>();
         blocks2.add(new Block("MO", "11:00", "14:00", ""));
         blocks2.add(new Block("WE", "12:30", "14:00", ""));
-        Section lec0201 = new Section("LEC-0201", "inst1", blocks2);
+        lec0201 = new Section("LEC-0201", "inst1", blocks2);
         sections.add(lec0201);
 
         List<Block> blocks3 = new ArrayList<>();
         blocks3.add(new Block("FR", "11:00", "12:00", ""));
-        Section tut0101 = new Section("TUT-0101", "inst1", blocks3);
+        tut0101 = new Section("TUT-0101", "inst1", blocks3);
         sections.add(tut0101);
 
         List<Block> blocks4 = new ArrayList<>();
         blocks4.add(new Block("TH", "11:00", "12:00", ""));
-        Section tut0201 = new Section("TUT-0201", "inst1", blocks4);
+        tut0201 = new Section("TUT-0201", "inst1", blocks4);
         sections.add(tut0201);
 
         List<Block> blocks5 = new ArrayList<>();
         blocks5.add(new Block("MO", "18:00", "20:00", ""));
-        Section pra0101 = new Section("PRA-0101", "inst1", blocks5);
+        pra0101 = new Section("PRA-0101", "inst1", blocks5);
         sections.add(pra0101);
 
-        CalendarCourse calCourse = new CalendarCourse("course", sections, "S",
+        calCourse = new CalendarCourse("course", sections, "S",
                 "COS111", "3");
-
-
+    }
+    @Test
+    void testGenerateAllTimetableCoursesWithoutArgument() {
         List<TimetableCourse> expected = new ArrayList<>();
 
         List<Section> sections1 = new ArrayList<>();
@@ -89,11 +98,44 @@ class TimetableCourseGeneratorTest {
         }
         List<TimetableCourse> actual = new TimetableCourseGenerator(calCourse).generateAllTimetableCourses();
         assertEquals(expected, actual);
-
     }
 
     @Test
     void testGenerateAllTimetableCoursesWithArgument() {
+        List<TimetableCourse> expected = new ArrayList<>();
 
+        List<Section> sections1 = new ArrayList<>();
+        sections1.add(lec0101);
+        sections1.add(tut0101);
+        sections1.add(pra0101);
+        try {
+            expected.add(new TimetableCourse("course", sections1, "S", "COS111", "3"));
+        } catch (InvalidSectionsException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<Section> sections3 = new ArrayList<>();
+        sections3.add(lec0201);
+        sections3.add(tut0101);
+        sections3.add(pra0101);
+        try {
+            expected.add(new TimetableCourse("course", sections3, "S", "COS111", "3"));
+        } catch (InvalidSectionsException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<Section> lectures = new ArrayList<>();
+        lectures.add(lec0101);
+        lectures.add(lec0201);
+
+        List<Section> tutorials = new ArrayList<>();
+        tutorials.add(tut0101);
+
+        List<Section> practicals = new ArrayList<>();
+        practicals.add(pra0101);
+
+        List<TimetableCourse> actual =
+                new TimetableCourseGenerator(calCourse).generateAllTimetableCourses(lectures, tutorials, practicals);
+        assertEquals(expected, actual);
     }
 }
