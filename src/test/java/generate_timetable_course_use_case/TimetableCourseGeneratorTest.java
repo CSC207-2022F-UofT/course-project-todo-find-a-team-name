1b67c5b17b129114004911fbeadaadb9c7ec4014
Kai15116 <kai.iharantonio@gmail.com>
2022-11-29 12:10:49 -0500

    Added test files and changed the name of the method to be consistent

---
+++ src/test/java/generate_timetable_course_use_case/TimetableCourseGeneratorTest.java
@@ -0,0 +1,18 @@
+package generate_timetable_course_use_case;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimetableCourseGeneratorTest {
+
+    @Test
+    void testGenerateAllTimetableCoursesWithoutArgument() {
+
+    }
+
+    @Test
+    void testGenerateAllTimetableCoursesWithArgument() {
+
+    }
+}
