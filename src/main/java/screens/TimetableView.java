853cef559db79b2e7aeea7306ecdc759b235d01d
Kai15116 <kai.iharantonio@gmail.com>
2022-11-30 01:50:33 -0500

    Extracted method to resolve long method

--- src/main/java/screens/TimetableView.java
+++ src/main/java/screens/TimetableView.java
@@ -21,6 +21,8 @@ public class TimetableView extends JPanel implements MouseListener {
     public static final int NUM_ROWS = (END_TIME - START_TIME) * 2 + 1;
     public static final int NUM_COLUMNS = 6;
     public static final String[] WEEK_DAYS = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    private int cellWidth;
+    private int cellHeight;
     private TimetableViewModel timetableViewModel;
     private final HashMap<String, Color> courseColors;
     private final List<TimetableViewEventListener> observers;
@@ -61,12 +63,28 @@ public class TimetableView extends JPanel implements MouseListener {
      */
     @Override
     public void paintComponent(Graphics g){
-
         Rectangle r = this.getBounds();
+        cellWidth = r.width / NUM_COLUMNS;
+        cellHeight = r.height / NUM_ROWS;
 
-        int cellWidth = r.width / NUM_COLUMNS;
-        int cellHeight = r.height / NUM_ROWS;
+        drawEmptyTimetable(g);
+        for (int i = 0; i < timetableViewModel.getCourseData().size(); i++){
+            TimetableViewCourseModel courseModel = timetableViewModel.getCourseData().get(i);
+            for (TimetableViewSectionModel sectionModel : courseModel.getSectionModels()){
+                for (TimetableViewBlockModel blockModel : sectionModel.getBlockModels()){
+                    drawBlock(g, blockModel, courseModel.getCode(), sectionModel.getCode());
+                }
+            }
+        }
+    }
 
+    /**
+     * Draw empty timetable with NUM_COLUMNS * NUM_ROWS rectangles of size cellWidth x cellHeight.
+     * It displays day of the week at the top row and time at the left column.
+     *
+     * @param g Graphics object used to draw
+     */
+    private void drawEmptyTimetable(Graphics g){
         for (int i = 0; i < NUM_COLUMNS; i++){
             for (int j = 0; j < NUM_ROWS; j++){
                 g.setColor(Color.DARK_GRAY);
@@ -86,40 +104,44 @@ public class TimetableView extends JPanel implements MouseListener {
                 if (j == 0){
                     g.setColor(Color.GRAY);
                     g.fillRect(x, y, cellWidth, cellHeight);
-                    g.setColor(Color.BLACK);
 
-                    if (i != 0)
+                    if (i != 0){
+                        g.setColor(Color.BLACK);
                         g.drawString(WEEK_DAYS[i - 1], x, y + cellHeight);
+                    }
                     g.setColor(Color.DARK_GRAY);
                 }
                 g.drawRect(x, y, cellWidth, cellHeight);
             }
         }
+    }
 
-        for (int i = 0; i < timetableViewModel.getCourseData().size(); i++){
-            TimetableViewCourseModel courseModel = timetableViewModel.getCourseData().get(i);
-            for (TimetableViewSectionModel sectionModel : courseModel.getSectionModels()){
-                for (TimetableViewBlockModel blockModel : sectionModel.getBlockModels()){
-                    int y1 = (int) ((blockModel.getStartTime() - START_TIME) * 2 * cellHeight) + cellHeight;
-                    int y2 = (int) ((blockModel.getEndTime() - START_TIME) * 2 *  cellHeight) + cellHeight;
-                    int x1 = (blockModel.getDay() + 1) * cellWidth;
+    /**
+     * Draw the given block as a rectangle in the appropriate position with the given course code and section code
+     * displayed inside the box.
+     *
+     * @param g Graphics object used to draw
+     * @param blockModel model storing all the information needed for displaying block
+     * @param courseCode code of the course for the given block
+     * @param sectionCode code of the section for the given block
+     */
+    private void drawBlock(Graphics g, TimetableViewBlockModel blockModel, String courseCode, String sectionCode){
+        int y1 = (int) ((blockModel.getStartTime() - START_TIME) * 2 * cellHeight) + cellHeight;
+        int y2 = (int) ((blockModel.getEndTime() - START_TIME) * 2 *  cellHeight) + cellHeight;
+        int x1 = (blockModel.getDay() + 1) * cellWidth;
 
-                    g.setColor(courseColors.get(courseModel.getCode()));
-                    g.fillRect(x1, y1, cellWidth, y2 - y1);
-                    String text = courseModel.getCode() + "; " + sectionModel.getCode();
+        g.setColor(courseColors.get(courseCode));
+        g.fillRect(x1, y1, cellWidth, y2 - y1);
+        String text = courseCode + "; " + sectionCode;
 
-                    g.setColor(Color.BLACK);
+        g.setColor(Color.BLACK);
 
-                    Font font = g.getFont();
-                    float fontSize = ((float) cellWidth / g.getFontMetrics(font).stringWidth(text)) * font.getSize();
-                    g.setFont(font.deriveFont(fontSize));
+        Font font = g.getFont();
+        float fontSize = ((float) cellWidth / g.getFontMetrics(font).stringWidth(text)) * font.getSize();
+        g.setFont(font.deriveFont(fontSize));
 
-                    g.drawString(text, x1, y1 + cellHeight);
-                    g.setFont(font);
-
-                }
-            }
-        }
+        g.drawString(text, x1, y1 + cellHeight);
+        g.setFont(font);
     }
 
     /**
