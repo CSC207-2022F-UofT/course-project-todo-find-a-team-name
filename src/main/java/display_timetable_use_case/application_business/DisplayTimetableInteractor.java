f3a5ba67fcd7b6d29599fb814276d1adffee7aa6
Kai15116 <kai.iharantonio@gmail.com>
2022-12-02 16:59:17 -0500

    Added comments

--- src/main/java/display_timetable_use_case/application_business/DisplayTimetableInteractor.java
+++ src/main/java/display_timetable_use_case/application_business/DisplayTimetableInteractor.java
@@ -4,14 +4,27 @@ import entities.Timetable;
 import retrieve_timetable_use_case.application_business.EntityConverter;
 import retrieve_timetable_use_case.application_business.TimetableModel;
 
+/**
+ * Interactor for display timetable use case, which communicates with the presenter to
+ * display the timetable
+ */
 public class DisplayTimetableInteractor implements DisplayTimetableInputBoundary{
     private Timetable timetable = null;
     private final DisplayTimetableOutputBoundary presenter;
 
+    /**
+     * Constructs the DisplayTimetableInteractor from the given presenter
+     *
+     * @param presenter presenter for display timetable use case
+     */
     public DisplayTimetableInteractor(DisplayTimetableOutputBoundary presenter) {
         this.presenter = presenter;
     }
 
+    /**
+     * Tells the presenter to prepare for displaying the timetable if timetable is not null.
+     * If timetable is null tell the presenter to prepare for displaying the fail view with error message.
+     */
     @Override
     public void displayTimetable() {
         if (timetable != null){
@@ -22,6 +35,11 @@ public class DisplayTimetableInteractor implements DisplayTimetableInputBoundary
         }
     }
 
+    /**
+     * Set the timetable to the given timetable
+     *
+     * @param timetable new timetable
+     */
     public void setTimetable(Timetable timetable) {
         this.timetable = timetable;
     }
--- src/main/java/display_timetable_use_case/frameworks_and_drivers/DisplayTimetablePresenter.java
+++ src/main/java/display_timetable_use_case/frameworks_and_drivers/DisplayTimetablePresenter.java
@@ -5,24 +5,48 @@ import display_timetable_use_case.application_business.DisplayTimetableOutputBou
 import retrieve_timetable_use_case.application_business.TimetableModel;
 import retrieve_timetable_use_case.interface_adapters.TimetableModelConverter;
 
+/**
+ * Presenter for displayTimetable use case that communicates with view to
+ * display the timetable (or error message) to the user
+ */
 public class DisplayTimetablePresenter implements DisplayTimetableOutputBoundary {
     private ITimetableUI view;
 
+    /**
+     * Constructs the DisplayTimetablePresenter from the given view
+     *
+     * @param view view that displays the timetable
+     */
     public DisplayTimetablePresenter(ITimetableUI view) {
         this.view = view;
     }
 
+    /**
+     * Convert the timetableModel to view model that is suitable for displaying the timetable, and
+     * pass that into the view to update the timetable.
+     *
+     * @param timetableModel model representing all information for the timetable
+     */
     @Override
     public void prepareTimetable(TimetableModel timetableModel) {
         TimetableViewModel viewModel = TimetableModelConverter.timetableToView(timetableModel);
         view.updateTimetable(viewModel);
     }
 
+    /**
+     * Tells the view to show given message to the user
+     * @param message message shown to the user
+     */
     @Override
     public void prepareFailView(String message) {
         view.showTimetableFailView(message);
     }
 
+    /**
+     * Set view to the given view
+     *
+     * @param view new view
+     */
     public void setView(ITimetableUI view){
         this.view = view;
     }
