867e74210fcf88d838091dcdd8f4b43cb19d40e1
Emily Su <emilyzqsu@gmail.com>
2022-12-08 09:23:13 -0500

    Updated SaveTimetableInteractor's onNext method

--- src/main/java/fileio_use_case/application_business/timetable_specific_classes/SaveTimetableInteractor.java
+++ src/main/java/fileio_use_case/application_business/timetable_specific_classes/SaveTimetableInteractor.java
@@ -2,13 +2,6 @@ package fileio_use_case.application_business.timetable_specific_classes;
 
 import entities.*;
 import fileio_use_case.frameworks_and_drivers.TimetableGateway;
-import retrieve_timetable_use_case.application_business.BlockModel;
-import retrieve_timetable_use_case.application_business.CourseModel;
-import retrieve_timetable_use_case.application_business.SectionModel;
-import retrieve_timetable_use_case.application_business.TimetableModel;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.Flow;
 
 public class SaveTimetableInteractor implements Flow.Subscriber<Object>{
@@ -17,34 +10,21 @@ public class SaveTimetableInteractor implements Flow.Subscriber<Object>{
     public SaveTimetableInteractor(TimetableGateway timetableGateway) {
         this.timetableGateway = timetableGateway;
     }
-    /** Reads timetableRequestModel into a JSON file to be stored in
-     * src/main/saved_timetables/.
-     * @param timetableRequestModel - a request model for Timetable
+
+    /** Method where classes this interactor is subscribed to will have their entity passed to
+     * to convert a timetable to file.
+     *
      */
-    public void timetableToFile(TimetableModel timetableRequestModel, String sessionType) throws InvalidSectionsException {
-        Timetable aTimetable = timetableModelToTimetable(timetableRequestModel, sessionType);
-        timetableGateway.timetableToFile(aTimetable);
+    public void updatingTimetableToFile(Timetable savedTimetable) {
+        timetableToFile(savedTimetable);
     }
-    /** Helper method for timetableToFile */
-    private Timetable timetableModelToTimetable(TimetableModel model, String sessionType) throws InvalidSectionsException {
-        ArrayList<TimetableCourse> allCourses = new ArrayList<>();
-        for (CourseModel course : model.getCourses()) {
-            allCourses.add(courseModelToTimetableCourse(course));
-        }
-        return new Timetable(allCourses, sessionType);
-    }
-    /** Helper method for helper method timetableModelTimetable */
-    private TimetableCourse courseModelToTimetableCourse(CourseModel course) throws InvalidSectionsException {
-        List<Section> allSections= new ArrayList<>();
-        for (SectionModel section : course.getSections()) {
-            List<Block> allBlocks= new ArrayList<>();
-            for (BlockModel block : section.getBlocks()){
-                allBlocks.add(new Block(String.valueOf(block.getDay()), String.valueOf((block.getStartTime())),
-                        String.valueOf(block.getEndTime()), String.valueOf(block.getRoom())));
-            }
-            allSections.add(new Section(section.getCode(), section.getInstructor(), allBlocks));
-        }
-        return new TimetableCourse(course.getTitle(), allSections, course.getCourseSession(), course.getCourseCode(), course.getBreadth());
+
+    /** Reads Timetable into a JSON file to be stored in
+     * src/main/saved_timetables/.
+     * @param timetableToSave - Timetable entity that user wants to save
+     */
+    public void timetableToFile(Timetable timetableToSave) {
+        timetableGateway.timetableToFile(timetableToSave);
     }
 
     /**
@@ -56,14 +36,16 @@ public class SaveTimetableInteractor implements Flow.Subscriber<Object>{
     }
 
     /**
-     * @param item the item
+     * Sends desired timetable to save to be converted into JSON file and saved in /src/main/saved_timetables
+     * @param timetable the timetable user wants to save (Should be a Timetable entity)
      */
     @Override
-    public void onNext(Object item) {
-
+    public void onNext(Object timetable) {
+        updatingTimetableToFile((Timetable) timetable);
     }
 
     /**
+     * Can't be removed. Part of Java Flow
      * @param throwable the exception
      */
     @Override
@@ -72,7 +54,7 @@ public class SaveTimetableInteractor implements Flow.Subscriber<Object>{
     }
 
     /**
-     *
+     * Can't be removed. Part of Java flow
      */
     @Override
     public void onComplete() {
